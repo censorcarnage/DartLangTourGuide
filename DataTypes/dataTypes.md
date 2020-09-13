4816092a91ea0c4857086592d23662cf874832a4
b0206962 <kartik.wadehra@airtelbank.com>
2020-09-13 18:55:53 +0530

    Datatypes until Runes and Graphemes

--- DataTypes/dataTypes.md
+++ DataTypes/dataTypes.md
@@ -6,3 +6,32 @@
 # Compile time constants
 1. const are used to declare compile time constants.
 2. RHS must also be compile time constants.
+
+# Strings
+1. both ' ' and " " are used for making strings.
+2. ${string} can be used to print strings. String interpolation.
+3. Concatenate strings using + operators.
+4. To create multi-line string used ''' or """.
+5. To create a raw string use r as a prefix.
+6. Copied straight from the tour - Literal strings are compile-time constants, as long as any interpolated expression is a compile-time constant that evaluates to null or a numeric, string, or boolean value.
+
+# Booleans
+1. bool as a type, true and false as objects, both are compile time constants.
+
+# Lists
+1. zero based indexing.
+2. Collection if and for.
+3. spread and null aware spread operators for adding collections to other collections.
+
+# Sets
+1. unordered collection of unique values.
+2. To create an empty set, use <Type>{} to create it.
+3. const prefix to create compile time constant.
+4. .length to find length.
+
+# Set or map? The syntax for map literals is similar to that for set literals. Because map literals came first, {} defaults to the Map type. If you forget the type annotation on {} or the variable it’s assigned to, then Dart creates an object of type Map<dynamic, dynamic>.
+
+# Maps
+
+1. key value type DS.
+2. two ways of creation -  type inference and constructor.
--- DataTypes/datatypes.dart
+++ DataTypes/datatypes.dart
@@ -10,6 +10,81 @@ main(List<String> args) {
 //Compile time constants
   const msPerSecond = 1000;
   const secondsUntilRetry = 5;
+  //only in which RHS also has compile time constants.
   const msUntilRetry = secondsUntilRetry * msPerSecond;
   print(msUntilRetry);
+
+//Strings
+  var s = "string interpolation";
+  print("Dart has $s");
+  var b = 'Hello Darkness';
+  var c = "My Old Friend";
+  var d = b + c;
+  print("$d");
+  //multi line strings
+  var f = '''This is a multi line string.
+I write on multiple lines''';
+  print("$f");
+  var g = r"This is a string \n has no special treatment";
+  print("$g");
+
+  const a = true;
+  const v = 12;
+  const m = 'a constant string';
+  const expre = '$a $v $m';
+  print(expre);
+
+  // These do NOT work in a const string.
+// var aNum = 0;
+// var aBool = true;
+// var aString = 'a string';
+// const aConstList = [1, 2, 3];
+
+//Booleans
+  var av = true;
+  print("$av");
+
+  //Lists
+
+  var li = [1, 2];
+  print(li[0]);
+
+//compile time constant list
+  var li1 = const [1, 2];
+  // li1[1] = 3; //Unhandled exception:Unsupported operation:
+  // Cannot modify an unmodifiable list
+  print(li1);
+
+  //spread and null aware spread
+  var li2 = [1, 2, 3];
+  var li3 = [...li2];
+  print(li3);
+
+  var li4;
+  var li5 = [0, ...?li4];
+  var li6 = [...?li4];
+  print(li5);
+  print(li6);
+
+  //Sets
+  var st = {"Hello", "Darkness", "my", "old", "friend"};
+  print(st);
+  // Empty set
+  var gh = <String>{};
+  print(gh);
+
+  //Maps
+  var nobleGases = {
+    2: 'helium',
+    10: 'neon',
+    18: 'argon',
+  };
+
+  var nobleGases1 = Map();
+  nobleGases1[2] = 'helium';
+  nobleGases1[10] = 'neon';
+  nobleGases1[18] = 'argon';
+
+  print(nobleGases);
+  print(nobleGases1);
 }
